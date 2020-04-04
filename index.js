93113e29eec9cc680fff1740f7d3282c28696242
fbzr <fabricio.spb@gmail.com>
2020-04-04 16:36:28 -0700

    implement get /api/users and post /api/users

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+const users = [];
+
 server.get('/', (req, res) => {
     res.json({ msg: 'success' });
 });
 
 // return an array users
 server.get('/api/users', (req, res) => {
-
+    try {
+        res.json(users);
+    } catch(err) {
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+    }
 });
 
 // creates an user using the information sent inside the request body
 server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
 
+    if(!name || !bio) {
+        res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+    }
+
+    const newUser = { name, bio };
+    users.push(newUser);
+    res.status(201).json(newUser);
 });
 
 // return the user object with the specified id
