14cea1097cabe16a01834d3c83ca7674098a7ec0
fbzr <fabricio.spb@gmail.com>
2020-04-05 11:30:10 -0700

    install and enable cors middleware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const uuid = require('uuid');
+const cors = require('cors');
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 const users = [];
 
